705d2607124f5b41547a1a6f4835f3a52fd97d19
imthedeputy <lahozdac@oregonstate.edu>
2017-05-28 11:21:56 -0400

    resending pair prog changes

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -32,15 +32,50 @@ public class UrlValidatorTest extends TestCase {
    private boolean printStatus = false;
    private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
    
-   String[] scheme = {"http://", "ftp://"};
-   String[] host = {"www.google.com", "amazon.com"};
+   UrlFragment[] scheme = {
+ 		  new UrlFragment(true,"https://"),
+ 		  new UrlFragment(false,"htp://")
+   };
+   UrlFragment[] host = {
+		  new UrlFragment(true,"amazon.com"),
+		  new UrlFragment(false,"amazon.")
+   };
+   UrlFragment[] port = {
+		  new UrlFragment(true,":800"),
+		  new UrlFragment(false,";800"),
+		  new UrlFragment(true,"")
+   };
+   UrlFragment[] query = {
+		  new UrlFragment(true,"?this=that"),
+		  new UrlFragment(false,"?thisthat"),
+		  new UrlFragment(true,"")
+   };
+   UrlFragment[] path = {
+		   new UrlFragment(true,"/index"),
+		   new UrlFragment(false,"\\index"),
+		   new UrlFragment(true,"")
+   };
+   UrlFragment[] fragment = {
+		   new UrlFragment(true,"#nav"),
+		   new UrlFragment(false,"&nav"),
+		   new UrlFragment(true,"")
+   };
    StringBuffer url = new StringBuffer();
-   StringBuffer result = new StringBuffer();
+   boolean expected = true;
    
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
+  public class UrlFragment {
+	  public boolean valid;
+	  public String urlFragment;
+	
+	  public UrlFragment(boolean v,String u) {
+		  valid = v;
+		  urlFragment = u;
+	  }
+  }
    
    
    public void testManualTest()
@@ -64,16 +99,40 @@ public class UrlValidatorTest extends TestCase {
    
    public void testIsValid()
    {
-	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+//	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   String[] validSchemes = {"https://","http://"};
+	   
+	   UrlValidator urlVal = new UrlValidator(validSchemes);
 
-	   for(int i = 0; i < scheme.length; i++)
-	   {
+	   for(int i = 0; i < scheme.length; i++) {
 		   
-		   for(int j = 0; j < host.length; j++){
-			   url.delete(0, url.length()); // clear the buffer
-			   url.append(scheme[i]).append(host[j]);
-			   System.out.println(urlVal.isValid(url.toString()) + " - " + url.toString());
-//			   System.out.println(url.toString());
+		   for(int j = 0; j < host.length; j++) {
+			   
+			   for(int k = 0; k < port.length; k++) {
+				   
+				   for(int l = 0; l < path.length; l++) {
+					   
+					   for(int m = 0; m < query.length; m++) {
+						   
+						   for(int n = 0; n < fragment.length; n++) {
+							   url.delete(0, url.length()); // clear the buffer
+							   url.append(scheme[i].urlFragment).append(host[j].urlFragment).append(port[k].urlFragment)
+							   .append(path[l].urlFragment).append(query[m].urlFragment).append(fragment[n].urlFragment);
+							   
+							   expected = (scheme[i].valid && host[j].valid && port[k].valid && path[l].valid && query[m].valid && fragment[n].valid); 
+							   
+							   boolean actual = urlVal.isValid(url.toString());
+							   
+//							   System.out.println(urlVal.isValid(url.toString()) + " - " + url.toString());
+//							   System.out.println(url.toString());
+							   
+							   if(actual != expected) {
+								   System.out.println("Actual: " + actual + " - " + url.toString() + " - Expected: " + expected);
+							   }
+						   }
+					   }
+				   }
+			   }
 		   }
 	   }
    }
